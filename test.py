55f36219e9e0a8cf6231569f73bef943fb3b115e
LiJian <lijianpku15@163.com>
2017-09-12 21:19:46 +0800

    test.py add test_password

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from functools import reduce
 
 test_email = '821972394@qq.com'
 test_username = "amuro"
+test_password = '123456'
 test_user = User.query.filter_by(email=test_email).first()
 print(type(test_user.role))
 print(test_user.role)
